432c76b3830cff1afb324c104877f91c8309d5f4
w3endigo <git.unthread684@passmail.net>
2025-01-03 21:04:21 +0100

    🎉 first commit

---
+++ src/main/java/isen/projet_dp_api/ProjetDpApiApplication.java
@@ -0,0 +1,13 @@
+package isen.projet_dp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetDpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/isen/projet_dp_api/ProjetDpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package isen.projet_dp_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
