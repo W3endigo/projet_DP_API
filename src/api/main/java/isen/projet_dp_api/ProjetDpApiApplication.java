6301af5afc909e68502f87ca9c01cd6e182890a4
W3endigo <github.com.baffling117@passmail.net>
2025-01-08 14:20:08 +0100

    ♻️ modification endpoint test

--- src/api/main/java/isen/projet_dp_api/ProjetDpApiApplication.java
+++ src/api/main/java/isen/projet_dp_api/ProjetDpApiApplication.java
@@ -2,14 +2,14 @@ package isen.projet_dp_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class ProjetDpApiApplication {
 
-    @RequestMapping("/")
+    @GetMapping("/hello")
     public String home() {
         return "Hello Docker World";
     }
