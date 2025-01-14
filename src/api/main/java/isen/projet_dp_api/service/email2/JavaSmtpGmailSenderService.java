cace9e0c542aee009b4bc1b29386cc127d560262
w3endigo <>
2025-01-14 14:11:32 +0100

    ✨ Add template and picture template usage

--- src/api/main/java/isen/projet_dp_api/service/email2/JavaSmtpGmailSenderService.java
+++ src/api/main/java/isen/projet_dp_api/service/email2/JavaSmtpGmailSenderService.java
@@ -1,15 +1,33 @@
 package isen.projet_dp_api.service.email2;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.context.Context;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 @Service
+@Log4j2
 public class JavaSmtpGmailSenderService {
 
-    @Autowired
-    private JavaMailSender emailSender;
+    private final JavaMailSender emailSender;
+
+    private final SpringTemplateEngine templateEngine;
+
+    public JavaSmtpGmailSenderService(JavaMailSender emailSender, SpringTemplateEngine templateEngine) {
+        this.emailSender = emailSender;
+        this.templateEngine = templateEngine;
+    }
 
     public void sendEmail(String toEmail, String subject, String body){
         SimpleMailMessage message = new SimpleMailMessage();
@@ -20,6 +38,42 @@ public class JavaSmtpGmailSenderService {
 
         emailSender.send(message);
 
-        System.out.println("Message sent successfully");
+        log.info("Message sent successfully");
+    }
+
+    public void sendEmailTemplate(String toEmail, String subject, String templateName, Context context) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+
+        String html = templateEngine.process(templateName, context);
+
+        helper.setTo(toEmail);
+        helper.setText(html, true);
+        helper.setSubject(subject);
+        helper.setFrom("labmanagerresearch@gmail.com");
+
+        emailSender.send(message);
+
+        log.info("MessageTemplate sent successfully");
+    }
+
+    public void sendEmailTemplatePicture(String toEmail, String subject, String templateName, Context context, String imagePath, String imageResourceName) throws MessagingException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+
+        String html = templateEngine.process(templateName, context);
+
+        helper.setTo(toEmail);
+        helper.setText(html, true);
+        helper.setSubject(subject);
+        helper.setFrom("labmanagerresearch@gmail.com");
+
+        // Add inline image
+        FileSystemResource res = new FileSystemResource(new File(imagePath));
+        helper.addInline(imageResourceName, res);
+
+        emailSender.send(message);
+
+        log.info("MessageTemplatePicture sent successfully");
     }
 }
