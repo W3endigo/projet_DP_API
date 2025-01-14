ad5ce388ab327abcd65274d290bcf12ac6d7350a
w3endigo <>
2025-01-14 11:16:46 +0100

    🚧 save working environment

--- src/api/main/java/isen/projet_dp_api/ProjetDpApiApplication.java
+++ src/api/main/java/isen/projet_dp_api/ProjetDpApiApplication.java
@@ -1,8 +1,11 @@
 package isen.projet_dp_api;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import isen.projet_dp_api.service.email2.JavaSmtpGmailSenderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,14 @@ import io.swagger.v3.oas.annotations.Operation;
 @RestController
 public class ProjetDpApiApplication {
 
+
+    private final JavaSmtpGmailSenderService senderService;
+
+
+    public ProjetDpApiApplication(JavaSmtpGmailSenderService senderService) {
+        this.senderService = senderService;
+    }
+
     @Operation(summary = "My endpoint", security = @SecurityRequirement(name = "bearerAuth"))
     @GetMapping("/api/hello")
     public String home() {
@@ -20,6 +31,12 @@ public class ProjetDpApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProjetDpApiApplication.class, args);
+    }
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail(){
+        senderService.sendEmail("labmanagerresearch@gmail.com","This is subject","This is email body");
     }
 
 }
