652b172459fbaf04d40c947f52e97ff3ae52e6b8
w3endigo <git.unthread684@passmail.net>
2025-02-18 18:55:28 +0100

    ✨ user get and update

--- src/api/main/java/isen/projet_dp_api/controller/user/UserController.java
+++ src/api/main/java/isen/projet_dp_api/controller/user/UserController.java
@@ -6,14 +6,45 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import isen.projet_dp_api.model.ApiException;
-import isen.projet_dp_api.model.RegisterRequestResponse;
+import isen.projet_dp_api.model.dto.EmailDTO;
 import isen.projet_dp_api.model.dto.UserDTO;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "User", description = "Endpoints for managing user-related operations")
 public interface UserController {
+
+    @PostMapping("/user")
+    @Operation(summary = "Get user by email", responses = {
+            @ApiResponse(responseCode = "200", description = "User found", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid user data", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ApiException.ErrorResponse.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "500", description = "Unexpected error occurred", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ApiException.ErrorResponse.class)
+            ))
+    })
+    ResponseEntity<UserDTO> getUser(@RequestBody EmailDTO emailDTO);
+
+    @PutMapping("/user")
+    @Operation(summary = "Update user", responses = {
+            @ApiResponse(responseCode = "200", description = "User updated", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid user data", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ApiException.ErrorResponse.class)
+            )),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content(schema = @Schema(implementation = ApiException.class))),
+            @ApiResponse(responseCode = "500", description = "Unexpected error occurred", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = ApiException.ErrorResponse.class)
+            ))
+    })
+    ResponseEntity<String> updateUser(@RequestBody @Valid UserDTO userDTO);
 
 }
