cbb8dface255065ab22a2f3b369f32994ce643cb
Poussin 144p <jonathan.besse@outlook.fr>
2025-02-21 17:22:55 +0100

    :construction: Ajout d'un lien Project et Company

--- src/api/main/java/isen/projet_dp_api/repository/ProjectCompaniesRepository.java
+++ src/api/main/java/isen/projet_dp_api/repository/ProjectCompaniesRepository.java
@@ -2,7 +2,6 @@ package isen.projet_dp_api.repository;
 
 import isen.projet_dp_api.model.dao.ProjectCompaniesDAO;
 import isen.projet_dp_api.model.dao.ProjectCompaniesId;
-import isen.projet_dp_api.model.dao.ProjectDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectCompaniesRepository extends JpaRepository<ProjectCompaniesDAO, ProjectCompaniesId> {
