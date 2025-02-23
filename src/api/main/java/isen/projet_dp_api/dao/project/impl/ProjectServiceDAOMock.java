8c6052c74f338b631b55fa7c9a7b36896fd268a0
Poussin 144p <jonathan.besse@outlook.fr>
2025-02-23 21:27:09 +0100

    :clown_face:

---
+++ src/api/main/java/isen/projet_dp_api/dao/project/impl/ProjectServiceDAOMock.java
@@ -0,0 +1,12 @@
+package isen.projet_dp_api.dao.project.impl;
+
+import isen.projet_dp_api.dao.project.ProjectServiceDAO;
+import isen.projet_dp_api.model.dao.ProjectDAO;
+
+public class ProjectServiceDAOMock implements ProjectServiceDAO {
+    @Override
+    public ProjectDAO createProject(ProjectDAO projectDAO) {
+        //TODO
+        return null;
+    }
+}
---
+++ src/api/main/java/isen/projet_dp_api/dao/projectscompanies/impl/ProjectCompaniesServiceDAOMock.java
@@ -0,0 +1,12 @@
+package isen.projet_dp_api.dao.projectscompanies.impl;
+
+import isen.projet_dp_api.dao.projectscompanies.ProjectCompaniesServiceDAO;
+import isen.projet_dp_api.model.dao.ProjectCompaniesDAO;
+
+public class ProjectCompaniesServiceDAOMock implements ProjectCompaniesServiceDAO {
+    @Override
+    public ProjectCompaniesDAO createProjectCompanies(ProjectCompaniesDAO projectCompaniesDAO) {
+        //TODO
+        return null;
+    }
+}
