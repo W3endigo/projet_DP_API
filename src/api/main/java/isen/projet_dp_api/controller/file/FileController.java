70ec9df3cd24d275fa5de33b535efb46251f2afa
w3endigo <>
2025-03-03 15:01:02 +0100

    ✨ add file download endpoint supporting multiple files

--- src/api/main/java/isen/projet_dp_api/controller/file/FileController.java
+++ src/api/main/java/isen/projet_dp_api/controller/file/FileController.java
@@ -7,11 +7,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import isen.projet_dp_api.model.ApiException;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Tag(name = "File", description = "Endpoints for managing file-related operations")
 public interface FileController {
@@ -34,4 +38,26 @@ public interface FileController {
                     ))
             }, security = @SecurityRequirement(name = "bearerAuth"))
     ResponseEntity<String> putFile(@RequestParam("file") MultipartFile file, @RequestParam("projectName") String projectName);
+
+    // download file endpoint that takes a list of file name and a projectName and then return a file or a zip,
+    // depending on the number of file required
+    @PostMapping(value = "/api/files/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @Operation(summary = "Download a file", description = "Download a file from the server",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "File downloaded successfully."),
+                    @ApiResponse(responseCode = "400", description = "Invalid file data", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiException.ErrorResponse.class)
+                    )),
+                    @ApiResponse(responseCode = "403", description = "Access denied", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiException.ErrorResponse.class)
+                    )),
+                    @ApiResponse(responseCode = "500", description = "Unexpected error occurred", content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiException.ErrorResponse.class)
+                    ))
+            }, security = @SecurityRequirement(name = "bearerAuth"))
+    ResponseEntity<InputStreamResource> getFile(@RequestBody List<String> fileNames, @RequestParam("projectName") String projectName);
+
 }
--- src/api/main/java/isen/projet_dp_api/controller/file/impl/FileControllerImpl.java
+++ src/api/main/java/isen/projet_dp_api/controller/file/impl/FileControllerImpl.java
@@ -1,13 +1,20 @@
 package isen.projet_dp_api.controller.file.impl;
 
+import com.google.common.net.HttpHeaders;
 import isen.projet_dp_api.controller.file.FileController;
 import isen.projet_dp_api.service.FileService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URLConnection;
+import java.util.List;
 
 @Log4j2
 @RestController
@@ -25,5 +32,29 @@ public class FileControllerImpl implements FileController {
         log.debug("Uploading file for project: {}", projectName);
         fileService.putFile(file, projectName);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<InputStreamResource> getFile(List<String> fileNames, String projectName) {
+        log.debug("Downloading file(s) : {} for project: {}", fileNames.toString(), projectName);
+        var content = fileService.getFile(fileNames, projectName);
+
+        if (fileNames.size() > 1) {
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + projectName + ".zip")
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM).body(new InputStreamResource(content));
+        } else {
+            var mediaType = URLConnection.guessContentTypeFromName(fileNames.getFirst());
+            if (!mediaType.contains("application")) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType(fileNames.getFirst()))
+                        .body(new InputStreamResource(content));
+            } else {
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + fileNames.getFirst())
+                        .contentType(MediaType.parseMediaType(MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE))
+                        .body(new InputStreamResource(content));
+            }
+        }
     }
 }
